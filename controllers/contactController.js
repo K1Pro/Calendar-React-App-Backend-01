3984157cdd224a8335203393d55f6731534ca2ef
K1Pro <support@k1pro.net>
2022-09-22 12:12:32 +0200

    Sorting functionality

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,17 +4,24 @@ const Contact = require(`${__dirname}/../models/contactModel`);
 exports.getAllContacts = async (req, res) => {
   try {
     // Build Query
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Contact.find(JSON.parse(queryStr));
+    let query = Contact.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // Execute Query
     const contacts = await query;
