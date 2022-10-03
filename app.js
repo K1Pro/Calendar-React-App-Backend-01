f42abef582b8b7c9e25dfb0ac941f9644a60d0d8
K1Pro <support@k1pro.net>
2022-10-03 12:42:23 +0200

    catching errors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const cors = require('cors');
 
 const contactRouter = require('./routes/contactRoutes');
@@ -25,24 +27,9 @@ app.use('/api/v2/contacts', contactRouter);
 
 // Handling unhandled routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`,
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,9 @@
 const Contact = require(`${__dirname}/../models/contactModel`);
 const APIFeatures = require(`${__dirname}/../utils/apiFeatures`);
+const catchAsync = require(`${__dirname}/../utils/catchAsync`);
 
 // Middleware
-exports.aliasSorted = (req, res, next) => {
+exports.aliasSorted = (req, res) => {
   req.query.limit = '1000';
   req.query.sort = 'LastName';
   // req.query.fields = 'FirstName,LastName'
@@ -10,149 +11,103 @@ exports.aliasSorted = (req, res, next) => {
 };
 
 // Route Handlers
-exports.getAllContacts = async (req, res) => {
-  try {
-    // Execute Query
-    const features = new APIFeatures(Contact.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const contacts = await features.query;
+exports.getAllContacts = catchAsync(async (req, res, next) => {
+  // Execute Query
+  const features = new APIFeatures(Contact.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const contacts = await features.query;
 
-    // Send response
-    res.status(200).json({
-      status: 'success',
-      results: contacts.length,
-      data: {
-        contacts,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  // Send response
+  res.status(200).json({
+    status: 'success',
+    results: contacts.length,
+    data: {
+      contacts,
+    },
+  });
+});
 
-exports.getContact = async (req, res) => {
-  try {
-    const contact = await Contact.findById(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        contact,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.getContact = catchAsync(async (req, res, next) => {
+  const contact = await Contact.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      contact,
+    },
+  });
+});
 
-exports.getContactLastName = async (req, res) => {
-  try {
-    console.log(req.params.LastName);
-    const contact = await Contact.find({ LastName: req.params.LastName });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        contact,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.getContactLastName = catchAsync(async (req, res, next) => {
+  console.log(req.params.LastName);
+  const contact = await Contact.find({ LastName: req.params.LastName });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      contact,
+    },
+  });
+});
 
-exports.createContact = async (req, res) => {
-  try {
-    const newContact = await Contact.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        contact: newContact,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({ status: 'fail', message: 'Invalid Data set' });
-  }
-};
+exports.createContact = catchAsync(async (req, res, next) => {
+  const newContact = await Contact.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      contact: newContact,
+    },
+  });
+});
 
-exports.updateContact = async (req, res) => {
-  try {
-    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: { contact },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.updateContact = catchAsync(async (req, res, next) => {
+  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: { contact },
+  });
+});
 
-exports.deleteContact = async (req, res) => {
-  try {
-    await Contact.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.deleteContact = catchAsync(async (req, res, next) => {
+  await Contact.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 // work on this with more appropriate queries
-exports.getContactStats = async (req, res) => {
-  try {
-    const stats = await Contact.aggregate([
-      {
-        $match: { EHV: { $gte: '0' } },
+exports.getContactStats = catchAsync(async (req, res, next) => {
+  const stats = await Contact.aggregate([
+    {
+      $match: { EHV: { $gte: '0' } },
+    },
+    {
+      $group: {
+        _id: null,
+        // _id: { $toUpper: '$FirstName' }, if you want stats grouped
+        totalNumberOfContacts: { $sum: 1 },
+        numContactsWithEHV: { $sum: 'EHV' },
+        avgEHV: { $avg: '$EHV' },
+        minEHV: { $min: '$EHV' },
+        maxEHV: { $max: '$EHV' },
       },
-      {
-        $group: {
-          _id: null,
-          // _id: { $toUpper: '$FirstName' }, if you want stats grouped
-          totalNumberOfContacts: { $sum: 1 },
-          numContactsWithEHV: { $sum: 'EHV' },
-          avgEHV: { $avg: '$EHV' },
-          minEHV: { $min: '$EHV' },
-          maxEHV: { $max: '$EHV' },
-        },
-      },
-      {
-        $sort: { FirstName: 1 },
-      },
-      {
-        // just an example
-        $match: { _id: { $ne: 'EASY' } },
-      },
-    ]);
+    },
+    {
+      $sort: { FirstName: 1 },
+    },
+    {
+      // just an example
+      $match: { _id: { $ne: 'EASY' } },
+    },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      data: { stats },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: { stats },
+  });
+});
