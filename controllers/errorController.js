93da7c0ddd28409a058e20b0484cc5160ecca70e
K1Pro <support@k1pro.net>
2022-10-12 21:25:14 +0200

    handling duplicate fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,7 +6,10 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const message = 'Duplicate field value: x. Please use another value!';
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
 };
 
 const sendErrorDev = (err, res) => {
