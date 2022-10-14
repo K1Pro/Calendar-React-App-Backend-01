1db273e497c58a98867a2c7aa89fd5001b24807f
K1Pro <support@k1pro.net>
2022-10-14 19:26:44 +0200

    user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,22 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
