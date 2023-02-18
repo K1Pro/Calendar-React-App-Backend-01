da7fd54d75eb638542fed7b24c26ed7540efb340
K1Pro <support@k1pro.net>
2023-02-18 01:10:18 +0100

    removed virtuals and updated model

--- models/contactModel.js
+++ models/contactModel.js
@@ -29,15 +29,19 @@ const contactSchema = new mongoose.Schema(
     LastEditDate: String,
     Notes: String,
     Policy1Type: String,
+    Policy1RenewDate: String,
     Policy1RenewMMDD: String,
     Policy1Number: String,
     Policy2Type: String,
+    Policy2RenewDate: String,
     Policy2RenewMMDD: String,
     Policy2Number: String,
     Policy3Type: String,
+    Policy3RenewDate: String,
     Policy3RenewMMDD: String,
     Policy3Number: String,
     Policy4Type: String,
+    Policy4RenewDate: String,
     Policy4RenewMMDD: String,
     Policy4Number: String,
     LastReviewDate: String,
@@ -99,34 +103,35 @@ const contactSchema = new mongoose.Schema(
   }
 );
 
-contactSchema.virtual('Policy1RenewDate').get(function () {
-  const thisYearA = new Date(Date.now()).toJSON().slice(0, 4);
-  const thisRenewalA = `${thisYearA}-${this.Policy1RenewMMDD}`;
-  if (this.Policy1RenewMMDD) {
-    return thisRenewalA;
-  }
-});
-contactSchema.virtual('Policy2RenewDate').get(function () {
-  const thisYearB = new Date(Date.now()).toJSON().slice(0, 4);
-  const thisRenewalB = `${thisYearB}-${this.Policy2RenewMMDD}`;
-  if (this.Policy2RenewMMDD) {
-    return thisRenewalB;
-  }
-});
-contactSchema.virtual('Policy3RenewDate').get(function () {
-  const thisYearC = new Date(Date.now()).toJSON().slice(0, 4);
-  const thisRenewalC = `${thisYearC}-${this.Policy3RenewMMDD}`;
-  if (this.Policy3RenewMMDD) {
-    return thisRenewalC;
-  }
-});
-contactSchema.virtual('Policy4RenewDate').get(function () {
-  const thisYearD = new Date(Date.now()).toJSON().slice(0, 4);
-  const thisRenewalD = `${thisYearD}-${this.Policy4RenewMMDD}`;
-  if (this.Policy4RenewMMDD) {
-    return thisRenewalD;
-  }
-});
+// These are examples of virtuals, actually used to use them
+// contactSchema.virtual('Policy1RenewDate').get(function () {
+//   const thisYearA = new Date(Date.now()).toJSON().slice(0, 4);
+//   const thisRenewalA = `${thisYearA}-${this.Policy1RenewMMDD}`;
+//   if (this.Policy1RenewMMDD) {
+//     return thisRenewalA;
+//   }
+// });
+// contactSchema.virtual('Policy2RenewDate').get(function () {
+//   const thisYearB = new Date(Date.now()).toJSON().slice(0, 4);
+//   const thisRenewalB = `${thisYearB}-${this.Policy2RenewMMDD}`;
+//   if (this.Policy2RenewMMDD) {
+//     return thisRenewalB;
+//   }
+// });
+// contactSchema.virtual('Policy3RenewDate').get(function () {
+//   const thisYearC = new Date(Date.now()).toJSON().slice(0, 4);
+//   const thisRenewalC = `${thisYearC}-${this.Policy3RenewMMDD}`;
+//   if (this.Policy3RenewMMDD) {
+//     return thisRenewalC;
+//   }
+// });
+// contactSchema.virtual('Policy4RenewDate').get(function () {
+//   const thisYearD = new Date(Date.now()).toJSON().slice(0, 4);
+//   const thisRenewalD = `${thisYearD}-${this.Policy4RenewMMDD}`;
+//   if (this.Policy4RenewMMDD) {
+//     return thisRenewalD;
+//   }
+// });
 
 // function obfuscate(Policy1RenewDate) {
 //   Policy1RenewDate = Policy1RenewDate.slice(5, 10);
